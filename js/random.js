70d714041d203c3a45a4d4d05c53d0a667239e5e
Javier Ramirez <apollo.infinity88@gmail.com>
2017-12-27 00:00:47 -0800

    Finally I got the random quote generator working. Now I must inegrate the twitter api to send out the tweet.

--- js/random.js
+++ js/random.js
@@ -11,16 +11,19 @@ is not necessary for this project and misses on some of the points of how javasc
 5. I could always do this project with Angular but what's the fun in that?!
 */
 
-
 var btn = document.getElementById("btn");
 var quote = document.getElementById("quote");
 var author = document.getElementById("author");
 
+window.onload = ajax;
+btn.addEventListener("click", ajax);
 
-btn.addEventListener("click", function() {
+
+
+
+function ajax(){
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://apolloinfinity.github.io/src/data/quotes.json');
-
     xhr.onload = function() {
         if(xhr.status >= 200 && xhr.status <= 400){
 
@@ -38,10 +41,38 @@ btn.addEventListener("click", function() {
 
 
     }
-
     xhr.send();
-    // quote.innerText = randAuth();
-});
+}
+
+
+// btn.addEventListener("click", function() {
+//     var xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'https://apolloinfinity.github.io/src/data/quotes.json');
+
+//     xhr.onload = function() {
+//         if(xhr.status >= 200 && xhr.status <= 400){
+
+//             //This variable holds the JSON items
+//             var trekQuotes = JSON.parse(xhr.responseText);
+//             var rand = Math.floor(Math.random() * trekQuotes.length);
+//             quote.innerText = trekQuotes[rand].quoteText;
+//             author.innerText = trekQuotes[rand].quoteAuthor;
+
+//             // console.log(trekQoutes[0].quoteText); //Test only
+
+//         } else {
+//             console.log("connected to server but with an error");
+//         }
+
+
+//     }
+
+//     xhr.send();
+    
+//     // quote.innerText = randAuth();
+// });
+
+
 
 
 
